4e17845f730a7021d6390b36ae634a6b31f301c7
Jon Leverkuhn <JonLeverkuhn@gmail.com>
2018-06-24 19:22:09 -0600

    Set song list background image.

--- app/src/main/java/com/example/android/giantsandpilgrims/SongList.java
+++ app/src/main/java/com/example/android/giantsandpilgrims/SongList.java
@@ -1,17 +1,34 @@
 package com.example.android.giantsandpilgrims;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SongList extends AppCompatActivity {
     String albumTextID;
+    ImageView backgroundImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_list_layout);
+
         Bundle bundle = getIntent().getExtras();
         albumTextID = bundle.getString("albumID");
+
+        getViewObjectIDs();
+        setViews();
+    }
+
+    public void getViewObjectIDs() {
+        backgroundImage=findViewById(R.id.background_image);
+    }
+
+    public void setViews() {
+        Resources resource = getResources();
+        backgroundImage.setImageResource(resource.getIdentifier("com.example.android.giantsandpilgrims.drawable/" + albumTextID + "_full", null, null));
     }
 
 }
